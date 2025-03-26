fc06f87d1f5432954276f7f72c32fe2c5b25ac4a
DipeshMPatel <dipeshpatel.mp@gmail.com>
2025-03-27 01:19:41 +0530

    Qualifying UI

--- src/app/race/[id]/qualifying.tsx
+++ src/app/race/[id]/qualifying.tsx
@@ -1,12 +1,18 @@
-import { View, Text } from 'react-native';
- import React from 'react';
- 
- const QualifyingResults = () => {
-   return (
-     <View>
-       <Text>QualifyingResults</Text>
-     </View>
-   );
- };
- 
- export default QualifyingResults;
+
+import { FlatList } from 'react-native';
+import React from 'react';
+import raceRankingResponse from '../../../../assets/data/race-rankings.json';
+import RankingListItem from '../../../components/RankingListItem';
+const raceRankings = raceRankingResponse.data.raceRankings.response;
+
+const QualifyingScreen = () => {
+  return (
+
+    <FlatList
+      data={raceRankings}
+      renderItem={({ item }) => <RankingListItem item={item} />}
+    />
+  );
+};
+
+export default QualifyingScreen ;
